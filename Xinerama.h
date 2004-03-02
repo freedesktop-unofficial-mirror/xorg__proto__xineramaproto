d26f4e0e82dd3946d7148ec33e3d7d657cbbb61b
Kaleb Keithley <kaleb@freedesktop.org>
2004-03-02 20:01:32 +0000

    bug #230 Looks like I fubared backing out the changes to this file. And     remove xineramaProto.h from this branch

--- Xinerama.h
+++ Xinerama.h
@@ -1,77 +1,48 @@
-/* $XdotOrg: xc/include/extensions/Xinerama.h,v 1.1.4.1 2003/12/18 22:59:34 kaleb Exp $ */
-/*
+/* $XFree86: xc/include/extensions/Xinerama.h,v 3.4 2003/12/18 05:48:06 dawes Exp $ */
 
-Copyright 2003  The Open Group
+#ifndef _Xinerama_h
+#define _Xinerama_h
 
-Permission to use, copy, modify, distribute, and sell this software and its
-documentation for any purpose is hereby granted without fee, provided that
-the above copyright notice appear in all copies and that both that
-copyright notice and this permission notice appear in supporting
-documentation.
+typedef struct {
+   int   screen_number;
+   short x_org;
+   short y_org;
+   short width;
+   short height;
+} XineramaScreenInfo;
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
-OPEN GROUP BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
-AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-Except as contained in this notice, the name of The Open Group shall not be
-used in advertising or otherwise to promote the sale, use or other dealings
-in this Software without prior written authorization from The Open Group.
-
-*/
-/* $XFree86: xc/include/extensions/Xinerama.h,v 3.3 2002/09/16 18:05:19 eich Exp $ */
-
- 
-#ifndef _XINERAMA_H
-#define _XINERAMA_H
- 
-#include <X11/Xfuncproto.h>
- 
 _XFUNCPROTOBEGIN
- 
+
 Bool XineramaQueryExtension (
-    Display *dpy,
-    int *event_basep,
-    int *error_basep
+   Display *dpy,
+   int     *event_base,
+   int     *error_base
 );
- 
+
 Status XineramaQueryVersion(
-    Display *dpy,
-    int *major_version,
-    int *minor_version
+   Display *dpy,
+   int     *major_versionp,
+   int     *minor_versionp
 );
- 
-Bool XineramaActive (
-    Display *dpy,
-    Window window
+
+Bool XineramaIsActive(Display *dpy);
+
+
+/* 
+   Returns the number of heads and a pointer to an array of
+   structures describing the position and size of the individual
+   heads.  Returns NULL and number = 0 if Xinerama is not active.
+  
+   Returned array should be freed with XFree().
+*/
+
+XineramaScreenInfo * 
+XineramaQueryScreens(
+   Display *dpy,
+   int     *number
 );
- 
-Status XineramaGetData(
-    Display *dpy,
-    Window window,
-    XRectangle **framebuffer_rects,
-    int *number_framebuffers
-);
- 
-Status XineramaGetCenterHint (
-    Display *dpy,
-    Window root,
-    int *x,
-    int *y
-);
- 
-Status XineramaSetCenterHint (
-    Display *dpy,
-    Window root,
-    int x,
-    int y
-);
- 
+
 _XFUNCPROTOEND
- 
-#endif /* _XINERAMA_H */
+
+#endif /* _Xinerama_h */
+
